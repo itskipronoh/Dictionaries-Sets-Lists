5e03657b47ecde7f9ef8724f6c70dc4c664d9c39
Gideon-kipronoh <kipronogideon68@gmail.com>
2024-09-10 16:45:59 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+
+# Create a set
+
+set1 = {"pop", "rock", "soul", "hard rock", "rock", "R&B", "rock", "disco"}
+set1
+
+# Convert list to set
+
+album_list = [ "Michael Jackson", "Thriller", 1982, "00:42:19", \
+              "Pop, Rock, R&B", 46.0, 65, "30-Nov-82", None, 10.0]
+album_set = set(album_list)             
+album_set
+
+# Dictionaries
+
+D={'a':0,'b':1,'c':2}
+
+# Create a sample dictionary
+
+release_year_dict = {"Thriller": "1982", "Back in Black": "1980", \
+                    "The Dark Side of the Moon": "1973", "The Bodyguard": "1992", 
+                    "Bat Out of Hell": "1977", "Their Greatest Hits (1971-1975)": "1976",
+                    "Saturday Night Fever": "1977", "Rumours": "1977"}
+release_year_dict
